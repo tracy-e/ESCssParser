eb9b883c5751e66e208453cc7baa64b731ac9588
tracy-e <tracy.cpp@gmail.com>
2012-11-12 20:50:12 +0800

    Initial Commit

---
+++ ESCSSParser/ESAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ESAppDelegate.h
+//  ESCSSParser
+//
+//  Created by Tracy E on 12-11-12.
+//  Copyright (c) 2012年 Tracy E. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ESAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ESCSSParser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ESCSSParser
+//
+//  Created by Tracy E on 12-11-12.
+//  Copyright (c) 2012年 Tracy E. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ESAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ESAppDelegate class]));
+    }
+}

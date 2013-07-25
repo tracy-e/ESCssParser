56a87209d4ebb648bcb0a2a27775272f663e1e49
EsoftMobile <tracy.cpp@gmail.com>
2013-07-25 20:57:05 +0800

    arc

--- ESCSSParser/ESCSSParser.h
+++ ESCSSParser/ESCSSParser.h
@@ -6,6 +6,11 @@
 //  Copyright (c) 2012 EsoftMobile.com. All rights reserved.
 //
 
+#if !__has_feature(objc_arc)
+#error ESCSSParser must be built with ARC.
+// You can turn on ARC for only ESCSSParser files by adding -fobjc-arc to the build phase for each of its files.
+#endif
+
 @interface ESCSSParser : NSObject{}
 
 - (NSDictionary *)parse:(NSString *)cssText;
